66724edcbf214d2dff6bab4fc22a153ee651edc3
Mike <mahathaway93@gmail.com>
2017-04-22 15:44:14 -0700

    modified seeds

--- db/seeds/2_blogs_tags.js
+++ db/seeds/2_blogs_tags.js
@@ -4,18 +4,18 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return knex('blogs_tags').insert([
         {
-                  id: 1,
-                  blog_id: 1,
-                  tag_id: 1,
-                  created_at: new Date(),
-                  updated_at: new Date()
+          id: 1,
+          blog_id: 1,
+          tag_id: 1,
+          created_at: new Date(),
+          updated_at: new Date()
         },
         {
-                  id: 2,
-                  blog_id: 2,
-                  tag_id: 2,
-                  created_at: new Date(),
-                  updated_at: new Date()
+          id: 2,
+          blog_id: 2,
+          tag_id: 2,
+          created_at: new Date(),
+          updated_at: new Date()
         },
       ])
     })
