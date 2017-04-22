ef63f2f8d2934f8eb701d17272fb57b17fb34ac5
Ian Harvey <ith.harvey@gmail.com>
2017-04-22 11:44:35 -0700

    completed users table migration

---
+++ db/migrations/20170422113443_create_users_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users',table => {
+    table.increments()
+    table.string('first_name').notNull()
+    table.string('email').notNull().unique()
+    table.specificType('hashed_password','char(60)').notNull()
+    table.timestamps(true,true)
+  })
+
+};
+
+exports.down = function(knex) {
+return knex.schema.dropTable('users')
+};
