085c4d663ccef9e82504b391449402c73354eb13
Ian Harvey <ith.harvey@gmail.com>
2017-04-23 14:57:22 -0700

    seed issue being revisited'

--- routes/blogs/index.js
+++ routes/blogs/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-<<<<<<< HEAD
 const routingFunctions = require('./routingFunctions.js')
 const utilityFunction = require('./utilityFunctions.js')
 const db = require('../../db')
@@ -25,29 +24,6 @@ router.get('/:id/comments', showBlogComments)
 router.post('/:id/comments', addBlogComment)
 
 function voteBlogComment(req,res,next){
-=======
-// const routingFunctions = require('routingFunctions.js')
-
-
-// router.get('/', showAllBlogs)
-// router.get('/new', showAddBlogForm)
-// router.post('/new', addBlog)
-//
-// router.get('/:id', showSingleBlog)
-//
-// //Vote,flag,tag a blog
-//   //may need to break up this route
-//     // ('/:id/vote')
-//     // ('/:id/flag')
-//     // ('/:id/tag')
-// router.put('/:id', modifyBlog)
-//
-// //Vote on a comment --> functions kept in seperate page
-// router.get('/:id/comments/:id', voteBlogComment)
-// router.get('/:id/comments', showBlogComments)
-// router.post('/:id/comments', addBlogComment)
-//
->>>>>>> session
 
 }
 
