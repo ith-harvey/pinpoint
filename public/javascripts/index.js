cacb70c18612f23b92a5e620b4fd2f66a5c646f0
Ian Harvey <ith.harvey@gmail.com>
2017-04-26 13:04:32 -0700

    same as where I was

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,18 +4,28 @@
 $(document).ready(function() {
 console.log('attached');
 
-
+// displays area where users can create tags
 $('#tag-no-exist-btn').click(function() {
   $('#tag-show-adition-group').fadeIn()
 })
 
+// displays an aditional tag field where users can create multiple tags
 $('#tag-add-another-btn').click(function() {
-  console.log('clicking!');
   $('.tag-text-input').append(
     '<label class="control-label"  for="name">New tag: </label><input type="text" id="name" name="name" placeholder="" class="input-xlarge"><input type="button" class="btn btn-default tag-edit-btn" value="edit">'
   )
 })
 
+$('arrow-up').click(function () {
+  console.log('clickup');
+
+})
+
+
+$('arrow-down').click(function () {
+  console.log('clickdown');
+
+})
 
 
 
