97d10e57d8dbcb6598e0f7d1b9b66cd4c1de87ed
Ian Harvey <ith.harvey@gmail.com>
2017-08-11 22:51:40 -0500

    voting and add a blog now produce an error

--- public/javascripts/validationfuncs.js
+++ public/javascripts/validationfuncs.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
-  $('#btn-addblog-no-uid').on("click", function() {
-    console.log('funciton run');
-    // $('.alert-header').attr("display", 'block').delay(8);
+  //not logged in header apears on add-blog && vote-arrow click
+  $('#btn-addblog-no-uid, .arrow-up-no-uid, .arrow-down-no-uid').on("click", function() {
     $('.alert-header').slideDown().css("display","block")
   })
 })
