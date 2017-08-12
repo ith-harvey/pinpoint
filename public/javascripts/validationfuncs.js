4626e8da246edc15de2e1ded7c6762c84d041305
Ian Harvey <ith.harvey@gmail.com>
2017-08-11 23:28:28 -0500

    individual blog show still needs some work

--- public/javascripts/validationfuncs.js
+++ public/javascripts/validationfuncs.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   //not logged in header apears on add-blog && vote-arrow click
-  $('#btn-addblog-no-uid, .arrow-up-no-uid, .arrow-down-no-uid').on("click", function() {
+  $('.no-uid-inactive').on("click", function() {
     $('.alert-header').slideDown().css("display","block")
   })
 })
